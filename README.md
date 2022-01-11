4dd359950648328c003d3e97a1c032ebba4050aa
Shyun <shyun.in304@gmail.com>
2022-01-11 16:42:38 +0900

    update snow clone coding

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
-# flutter1
-
-A new Flutter project.
-
-## Getting Started
-
-This project is for flutter study.
-
-2022년 1월 3일부터 진행중! 
+# flutterStudy
+ projects with flutter
---
+++ snow/lib/config.dart
@@ -0,0 +1,10 @@
+import 'package:camera_deep_ar/camera_deep_ar.dart';
+
+DeepArConfig config = DeepArConfig(
+androidKey:
+"60a061eef57b96df93604bf8917b611207667af5ce55f48bcc075783feb37b972ed8e66111a4cfb5",
+ioskey:
+"60a061eef57b96df93604bf8917b611207667af5ce55f48bcc075783feb37b972ed8e66111a4cfb5",
+displayMode: DisplayMode.camera,
+// displayMode: DisplayMode.camera,
+);
